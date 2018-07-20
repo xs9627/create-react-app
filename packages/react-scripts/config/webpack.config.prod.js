dc6b833c98055eb1035dfeaf85b3843b00f734f2
Sonic Xiang <sonic.xiang@activenetwork.com>
2018-07-20 11:19:04 +0800

    Add background entry

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -97,7 +97,10 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: shouldUseSourceMap ? 'source-map' : false,
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: {
+    main: [require.resolve('./polyfills'), paths.appIndexJs],
+    background: paths.backgroundJs
+  },
   output: {
     // The build folder.
     path: paths.appBuild,
